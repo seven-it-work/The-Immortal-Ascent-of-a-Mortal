62dfe9106feedd70efa999e4f6cf67f9883e9e75
1399435579@qq.com <1399435579@qq.com>
2024-09-15 21:58:09 +0800

    初步构造战斗思路

--- src/obj/Fight.ts
+++ src/obj/Fight.ts
@@ -1,12 +1,94 @@
 import People from "./People";
 import Chance from "chance"
 
+export default class RoundFunction {
+    private readonly startRound: number;
+    private readonly endRound: number;
+    private readonly maximumNumberOfExecutions: number;
+    private readonly fight: Fight;
+    private readonly targetPeopleId: string[];
+    private readonly tempData: { any } = {};
+    private theNumberOfCurrentExecutions: number = 0;
+    private _isEnd: boolean = false;
+
+    currentFunc: (roundFunction: RoundFunction) => {}
+    overFunc: (roundFunction: RoundFunction) => {}
+
+
+    /**
+     *
+     * @param startRound 开始回合
+     * @param ongoingTurns 持续回合
+     * @param maximumNumberOfExecutions 最大执行次数
+     * @param fight fight对象
+     * @param targetPeopleId 执行模板对象id
+     * @param currentFunc 当前回合执行func
+     * @param overFunc 结束后的回收func
+     */
+    constructor(startRound: number, ongoingTurns: number, maximumNumberOfExecutions: number, fight: Fight, targetPeopleId: string[], currentFunc: () => {}, overFunc: () => {}) {
+        this.startRound = startRound;
+        this.endRound = startRound + ongoingTurns;
+        this.maximumNumberOfExecutions = maximumNumberOfExecutions;
+        this.fight = fight;
+        this.targetPeopleId = targetPeopleId;
+        this.currentFunc = currentFunc;
+        this.overFunc = overFunc;
+    }
+
+    public currentFunction(round: number) {
+        if (round >= this.endRound) {
+            this.overFunction();
+        }
+        if (this.theNumberOfCurrentExecutions > this.maximumNumberOfExecutions) {
+            this._isEnd = true;
+            return
+        }
+        this.theNumberOfCurrentExecutions++;
+        this.currentFunc(this)
+    }
+
+    public isEnd(): boolean {
+        return this._isEnd;
+    }
+
+    private overFunction() {
+        this._isEnd = true;
+        this.overFunc(this)
+    }
+}
+
 export default class Fight {
     defendTeamID: string[];
     attackingTeamID: string[];
     idAndPeopleMap: Map<string, People>
+    idAndRoundFunction: Map<string, RoundFunction>
+    currentRound: number;
 
-    public autoFight() {
+
+    constructor(defendTeam: People[], attackingTeam: People[]) {
+        this.defendTeamID = defendTeam.map(item => item.id);
+        this.attackingTeamID = attackingTeam.map(item => item.id);
+        this.idAndPeopleMap = new Map();
+        this.idAndRoundFunction = new Map();
+        [...defendTeam, ...attackingTeam].forEach(item => {
+            this.idAndPeopleMap.set(item.id, item)
+        })
+        this.currentRound = 0;
+    }
+
+    public doFight() {
+        this.currentRound++;
+        // 先执行加成
+        Array.from(this.idAndRoundFunction.values())
+            .filter(item => !item.isEnd())
+            .forEach(item => {
+                item.currentFunction(this.currentRound)
+            })
+        // 在进行对战
+        this.autoFight();
+    }
+
+    private autoFight() {
         // 获取idAndPeopleMap.values()按着速度进行排序
         Array.from(this.idAndPeopleMap.values()).sort((v1, v2) => {
             return v1.velocity - v2.velocity;
@@ -34,7 +116,7 @@ export default class Fight {
                     } else {
                         // 从中选一个
                         const pickone = new Chance().pickone(targetIdList);
-                        Fight.doFight(item, pickone)
+                        Fight.peopleFight(item, pickone, this)
                     }
                 }
 
@@ -42,8 +124,13 @@ export default class Fight {
         })
     }
 
-    public static doFight(attack: People, defend: People) {
+    private static peopleFight(attack: People, defend: People, fight: Fight) {
         // 判断是使用技能还是普通攻击。后续可以做一个比例 按比例来随机触发 todo
+        Fight.技能攻击(attack, defend, fight);
+        Fight.普通攻击(attack, defend, fight);
+    }
+
+    private static 普通攻击(attack: People, defend: People, fight: Fight) {
         // 普通攻击
         attack.usePhysical();
         // 普通攻击伤害=攻击力+装备攻击力加成 todo
@@ -56,5 +143,12 @@ export default class Fight {
 
         // 伤害赋值
         defend.blood.current -= harm
+    }
+
+    private static 技能攻击(attack: People, defend: People, fight: Fight) {
+        // 过滤出可以执行的技能 消耗元气<attack的元气 todo
+        // 如果一个技能都没有，执行普通攻击兜底。否则随机选择一个技能进行执行 todo
+
+
     }
 }
